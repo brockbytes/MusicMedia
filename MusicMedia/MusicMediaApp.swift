254169253da937bc05f46347c455354cb720471c
Brockton Carnes <brockbytes@gmail.com>
2025-03-13 19:04:00 -0400

    Initial Commit

---
+++ MusicMedia/MusicMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicMediaApp.swift
+//  MusicMedia
+//
+//  Created by Brockton Carnes on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicMediaTests/MusicMediaTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicMediaTests.swift
+//  MusicMediaTests
+//
+//  Created by Brockton Carnes on 3/13/25.
+//
+
+import Testing
+@testable import MusicMedia
+
+struct MusicMediaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
